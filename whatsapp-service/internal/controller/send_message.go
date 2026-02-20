4bc3900ac897f9d662aa8fa50c13087e796948f7
andersonmarin <anderson.marin@outlook.com>
2026-02-20 08:51:00 -0300

    Support media message processing in `SendMessage` and add helper functions for media uploads

--- whatsapp-service/internal/controller/send_message.go
+++ whatsapp-service/internal/controller/send_message.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"context"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v5"
@@ -11,7 +13,7 @@ import (
 func (ctrl *Controller) SendMessage(ctx *echo.Context) error {
 	var req struct {
 		To      string
-		Message string
+		Message *waE2E.Message
 	}
 	if err := ctx.Bind(&req); err != nil {
 		return err
@@ -21,7 +23,7 @@ func (ctrl *Controller) SendMessage(ctx *echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "to is required")
 	}
 
-	if req.Message == "" {
+	if req.Message == nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "message is required")
 	}
 
@@ -36,13 +38,93 @@ func (ctrl *Controller) SendMessage(ctx *echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "contact is not on WhatsApp")
 	}
 
-	message := waE2E.Message{
-		Conversation: &req.Message,
+	if err = fulfillMessage(ctx.Request().Context(), client, req.Message); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 
-	if _, err = client.SendMessage(ctx.Request().Context(), contacts[0].JID, &message); err != nil {
+	if _, err = client.SendMessage(ctx.Request().Context(), contacts[0].JID, req.Message); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 
 	return ctx.NoContent(http.StatusOK)
+}
+
+func fulfillMessage(ctx context.Context, client *whatsmeow.Client, message *waE2E.Message) error {
+	var url string
+	var mediaType whatsmeow.MediaType
+	if i := message.ImageMessage; i != nil {
+		url = *i.URL
+		mediaType = whatsmeow.MediaImage
+	} else if v := message.VideoMessage; v != nil {
+		url = *v.URL
+		mediaType = whatsmeow.MediaVideo
+	} else if a := message.AudioMessage; a != nil {
+		url = *a.URL
+		mediaType = whatsmeow.MediaAudio
+	} else if d := message.DocumentMessage; d != nil {
+		url = *d.URL
+		mediaType = whatsmeow.MediaDocument
+	} else {
+		return nil
+	}
+
+	r, err := downloadFromURL(ctx, url)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.UploadReader(ctx, r, nil, mediaType)
+	if err != nil {
+		return err
+	}
+
+	if i := message.ImageMessage; i != nil {
+		i.URL = &resp.URL
+		i.DirectPath = &resp.DirectPath
+		i.MediaKey = resp.MediaKey
+		i.FileEncSHA256 = resp.FileEncSHA256
+		i.FileSHA256 = resp.FileSHA256
+		i.FileLength = &resp.FileLength
+	} else if v := message.VideoMessage; v != nil {
+		v.URL = &resp.URL
+		v.DirectPath = &resp.DirectPath
+		v.MediaKey = resp.MediaKey
+		v.FileEncSHA256 = resp.FileEncSHA256
+		v.FileSHA256 = resp.FileSHA256
+		v.FileLength = &resp.FileLength
+	} else if a := message.AudioMessage; a != nil {
+		a.URL = &resp.URL
+		a.DirectPath = &resp.DirectPath
+		a.MediaKey = resp.MediaKey
+		a.FileEncSHA256 = resp.FileEncSHA256
+		a.FileSHA256 = resp.FileSHA256
+		a.FileLength = &resp.FileLength
+	} else if d := message.DocumentMessage; d != nil {
+		d.URL = &resp.URL
+		d.DirectPath = &resp.DirectPath
+		d.MediaKey = resp.MediaKey
+		d.FileEncSHA256 = resp.FileEncSHA256
+		d.FileSHA256 = resp.FileSHA256
+		d.FileLength = &resp.FileLength
+	}
+
+	return nil
+}
+
+func downloadFromURL(ctx context.Context, url string) (io.Reader, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	return res.Body, nil
 }
